7e73d226813965fe6cf328ba7a9ff09b5b387188
iliko13 <107374131+iliko13@users.noreply.github.com>
2022-10-21 16:46:03 +0400

    java

---
+++ script.js
@@ -0,0 +1,19 @@
+var income = 10000;
+if (income < 1500) {
+    alert("დაბალი ხელფასი")
+}
+else if (income >=1500 && income <=3500) {
+    alert("საშუალო ხელფასი")
+}
+else {
+    alert("მაღალი ხელფასი")
+}
+
+
+
+let number = 4
+if(number % 2 === 0){
+    console.log(`${number} არის ლუწი`)
+}else{
+    console.log(`${number} არის კენტი`)
+}
